dd9c7d90eb8aecb71f9089e76c064f883a2d865d
Illinois_Roadbuff <illinois_roadbuff@proton.me>
2025-12-23 07:37:15 +0100

    Update src/outside-scope.py
    Signed-off-by: Illinois_Roadbuff <illinois_roadbuff@proton.me>

--- src/outside-scope.py
+++ src/outside-scope.py
@@ -1,3 +1,7 @@
+# MIT License
+# Illinois_Roadbuff "Rogue Jr"
+# Usage: ./src/outside-scope.py
+
 import warnings
 
 warnings.warn("how are you running this- you got the complete wrong language. what type of package manager are you using????")
